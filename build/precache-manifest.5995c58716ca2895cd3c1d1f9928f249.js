c4ff7fccab11dafe476b457d30954546f108d3b7
冯菲珂 <33412300+Fengfeike@users.noreply.github.com>
2019-10-15 08:50:39 +0800

    build

---
+++ build/precache-manifest.5995c58716ca2895cd3c1d1f9928f249.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a4bd9487efe58bb77ddf68b702188c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a8ec6dfef5ec69a9bd0",
+    "url": "/static/css/main.1f3d1ad5.chunk.css"
+  },
+  {
+    "revision": "4fda6146352e6b874851",
+    "url": "/static/js/2.9805bce2.chunk.js"
+  },
+  {
+    "revision": "9a8ec6dfef5ec69a9bd0",
+    "url": "/static/js/main.cbfe98ca.chunk.js"
+  },
+  {
+    "revision": "213fba594b0d8051af55",
+    "url": "/static/js/runtime-main.ba8d901f.js"
+  }
+]);
